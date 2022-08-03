abe9f6792e94056623fb5d256cbe12210f9fbe8a
Trevor Covington <tcovington189@gmail.com>
2022-08-03 07:48:04 -0700

    Updating base

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import checker from 'vite-plugin-checker'
+
+/** @type {import('vite').UserConfig} */
 export default {
   plugins: [checker({ typescript: true })], // e.g. use TypeScript check
   base: '/sprite-animations-in-javascript/'
